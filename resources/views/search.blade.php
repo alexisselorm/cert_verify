3898d5bd21970930822293bb40a098cf9b065bb2
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-05-23 23:18:47 +0100

    Scout Import import and Filterable attributes bash added

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -14,7 +14,7 @@
                 @endif
                 <div>
                     <form action="/search" method="GET">
-                        <input  minlength="5" value="{{ request()->get('query') }}" type="search" name="query"
+                        <input minlength="5" value="{{ request()->get('query') }}" type="search" name="query"
                             placeholder="Type a certificate number" style="width:100%">
                         <button>Search</button>
                     </form>
@@ -23,19 +23,39 @@
                             @if ($students->count())
                                 @foreach ($students as $student)
                                     <div>
-                                        <h1>{{ $student->program->short_name }}</h1>
-                                        <h1>{{ $student->cgpa }}</h1>
-                                        @if ($student->cgpa >= 3.6)
-                                            <h1>First Class</h1>
-                                        @elseif ($student->cgpa >= 3.0)
-                                            <h1>Upper</h1>
-                                        @elseif ($student->cgpa >= 2.5)
-                                            <h1>Lower</h1>
-                                        @elseif ($student->cgpa >= 2.0)
-                                            <h1>Third Class</h1>
-                                        @else
-                                            <h1>Pass</h1>
-                                        @endif
+                                        <h1><u>CONFIRMATION OF CERTIFICATE - {{ $student->lname }}
+                                                {{ $student->fname }}</u></h1>
+
+                                        <p>This confirms that {{ $student->fname }} {{ $student->lname }}
+                                            ({{ $student->regno }})
+                                            is a past student of the University of Cape Coast. He
+                                            was admitted in {{ substr($student->doa, -4) }} to pursue a
+                                            {{ $student->program->program_type->comment }} in
+                                            {{ $student->program->long_name }}.
+                                        </p>
+                                        <p>{{ $student->lname }} wrote and passed the final examinations in
+                                            {{ substr($student->doc, -4) }} as was accordingly admitted to the degree of
+                                            {{ $student->program->program_type->comment }}
+                                            @if ($student->program->program_type->comment === 'BACHELOR')
+                                                <span>with
+                                                    @if ($student->cgpa >= 3.6)
+                                                        <span>First Class Honours</span>
+                                                    @elseif ($student->cgpa >= 3.0)
+                                                        <span>Second Upper, Upper Divison</span>
+                                                    @elseif ($student->cgpa >= 2.5)
+                                                        <span>Second Lower, Lower Division</span>
+                                                    @elseif ($student->cgpa >= 2.0)
+                                                        <span>Third Class</span>
+                                                    @else
+                                                        <span> a Pass</span>
+                                                    @endif
+                                                </span>
+                                            @else
+                                                <span>----INSERT date of completion here ----</span>
+                                            @endif
+                                        </p>
+
+
 
                                     </div>
                                 @endforeach
--- scoutImport.sh
+++ scoutImport.sh
@@ -2,3 +2,11 @@ php artisan scout:import "App\Models\Students"
 php artisan scout:import "App\Models\Program"
 php artisan scout:import "App\Models\Program_Type"
 php artisan scout:import "App\Models\Program_RunType"
+
+curl   -X POST 'http://localhost:7700/indexes/students/settings/searchable-attributes'   -H 'Content-type:application/json'   --data-binary '[
+        "cert_no"
+  ]'
+curl   -X POST 'http://localhost:7700/indexes/students/settings/filterable-attributes'   -H 'Content-type:application/json'   --data-binary '[
+        "cert_no",
+        "program_id"
+  ]'
